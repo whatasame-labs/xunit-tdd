a16dc7100eae3befd88709e4c77eae76c0983846
whatasame <97666463+whatasame@users.noreply.github.com>
2023-11-29 19:38:28 +0900

    feat: Run TestSuite introduced composite pattern

--- src/xunit/XUnitTest.java
+++ src/xunit/XUnitTest.java
@@ -8,6 +8,18 @@ public class XUnitTest {
 
         testSuite.run(testResult);
 
-        System.out.println(testResult.getSummary()); // TODO: Log failed test method names.
+        System.out.println(testResult.getSummary());
+
+        // ---
+
+        final TestSuite testSuite2 = new TestSuite();
+        testSuite2.add(new TestCaseTest("testTemplateMethod"));
+        testSuite2.add(new TestCaseTest("testResult"));
+        testSuite2.add(testSuite); // Composite pattern
+
+        final TestResult testResult2 = new TestResult();
+        testSuite2.run(testResult2);
+
+        System.out.println(testResult2.getSummary());
     }
 }
