2bedf6e0963a86cf897236c9a2d7d524b8e89451
whatasame <97666463+whatasame@users.noreply.github.com>
2023-11-29 18:38:04 +0900

    refactor: Refactor bootstrap to run tests dynamically

--- src/xunit/XUnitTest.java
+++ src/xunit/XUnitTest.java
@@ -1,11 +1,21 @@
 package xunit;
 
+import java.util.Set;
+
 public class XUnitTest {
 
     public static void main(String[] args) {
-        new TestCaseTest("testTemplateMethod").run();
-        new TestCaseTest("testResult").run();
-        new TestCaseTest("testFailedResultFormatting").run();
-        new TestCaseTest("testFailedResult").run();
+        final Set<String> testMethods = Set.of(
+                "testTemplateMethod",
+                "testResult",
+                "testFailedResultFormatting",
+                "testFailedResult"
+        );
+
+        for (String testMethod : testMethods) {
+            final TestResult testResult = new TestCaseTest(testMethod).run();
+
+            System.out.println(testMethod + "\n\t -> " + testResult.getSummary());
+        }
     }
 }
