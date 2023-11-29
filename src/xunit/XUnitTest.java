4be5c077ffc64e778ac95407bd8b008da6dcc884
whatasame <97666463+whatasame@users.noreply.github.com>
2023-11-29 18:00:41 +0900

    test: Test summary formatting with success and failed

--- src/xunit/XUnitTest.java
+++ src/xunit/XUnitTest.java
@@ -5,5 +5,6 @@ public class XUnitTest {
     public static void main(String[] args) {
         new TestCaseTest("testTemplateMethod").run();
         new TestCaseTest("testResult").run();
+        new TestCaseTest("testFailedResultFormatting").run();
     }
 }
