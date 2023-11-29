606cc3e9342a7125c4be447faa50b5affda3b719
whatasame <97666463+whatasame@users.noreply.github.com>
2023-11-29 18:34:00 +0900

    feat: Implement collecting test results

--- src/xunit/TestCaseTest.java
+++ src/xunit/TestCaseTest.java
@@ -2,24 +2,23 @@ package xunit;
 
 public class TestCaseTest extends TestCase {
 
-    private WasRun wasRun;
-
     public TestCaseTest(final String name) {
         super(name);
     }
 
-    @Override
-    public void setUp() {
-        this.wasRun = new WasRun("testMethod");
-    }
-
     public void testTemplateMethod() {
+        final WasRun wasRun = new WasRun("testMethod");
+
         wasRun.run();
+
         Assert.assertEquals("setUp testMethod tearDown", wasRun.getLog());
     }
 
     public void testResult() {
+        final WasRun wasRun = new WasRun("testMethod");
+
         final TestResult result = wasRun.run();
+
         Assert.assertEquals("1 run, 0 failed", result.getSummary());
     }
 
@@ -31,4 +30,14 @@ public class TestCaseTest extends TestCase {
 
         Assert.assertEquals("1 run, 1 failed", result.getSummary());
     }
+
+    public void testFailedResult() {
+        final WasRun wasRun = new WasRun("testBrokenMethod");
+
+        final TestResult result = wasRun.run();
+
+        Assert.assertEquals("1 run, 1 failed", result.getSummary());
+    }
+
+    // TODO: If there is no test method matching name, is it guaranteed that tearDown() is run?
 }
