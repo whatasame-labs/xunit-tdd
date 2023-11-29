e8b7ec8b8df544119939ac3df707d8491a6bc3c6
whatasame <97666463+whatasame@users.noreply.github.com>
2023-11-29 11:53:50 +0900

    feat: Run specific test method by name

--- src/xunit/WasRun.java
+++ src/xunit/WasRun.java
@@ -1,18 +1,32 @@
 package xunit;
 
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+
 public class WasRun {
 
+    private final String name;
     private boolean wasRun; // Collect test was run
 
-    public WasRun() {
+    public WasRun(final String name) {
         this.wasRun = false;
-    }
-
-    public void testMethod() {
-        this.wasRun = true;
+        this.name = name;
     }
 
     public boolean wasRun() {
         return wasRun;
+    }
+
+    public void run() {
+        try {
+            final Method method = this.getClass().getMethod(name);
+            method.invoke(this);
+        } catch (NoSuchMethodException | IllegalAccessException | InvocationTargetException e) {
+            throw new IllegalArgumentException("No such method: " + name, e);
+        }
+    }
+
+    public void testMethod() {
+        this.wasRun = true;
     }
 }
--- src/xunit/XUnitTest.java
+++ src/xunit/XUnitTest.java
@@ -3,10 +3,10 @@ package xunit;
 public class XUnitTest {
 
     public static void main(String[] args) {
-        final WasRun wasRun = new WasRun();
+        final WasRun wasRun = new WasRun("testMethod");
 
         System.out.println(wasRun.wasRun()); // false
-        wasRun.testMethod();
+        wasRun.run();
         System.out.println(wasRun.wasRun()); // true
     }
 }
