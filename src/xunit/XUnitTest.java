e4b111590c52d7ca1020136c2eb6e31be5cdf66a
whatasame <97666463+whatasame@users.noreply.github.com>
2023-11-29 19:07:35 +0900

    feat: Generate TestSuite in TestCaseTest

--- src/xunit/XUnitTest.java
+++ src/xunit/XUnitTest.java
@@ -3,14 +3,9 @@ package xunit;
 public class XUnitTest {
 
     public static void main(String[] args) {
-        final TestSuite testSuite = new TestSuite();
-        testSuite.add(new TestCaseTest("testTemplateMethod"));
-        testSuite.add(new TestCaseTest("testResult"));
-        testSuite.add(new TestCaseTest("testFailedResultFormatting"));
-        testSuite.add(new TestCaseTest("testFailedResult"));
-        testSuite.add(new TestCaseTest("testSuite"));
-
+        final TestSuite testSuite = TestCaseTest.suite();
         final TestResult testResult = new TestResult();
+
         testSuite.run(testResult);
 
         System.out.println(testResult.getSummary()); // TODO: Log failed test method names.
